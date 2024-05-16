a3fb6e60f3c1cf4fead0641f83958c506d47a2df
russelpwq <2201319@sit.singaporetech.edu.sg>
2024-05-16 10:14:14 +0800

    Initial Commit

---
+++ PKFAuditManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PKFAuditManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PKFAuditManagement/Program.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PKFAuditManagement.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Change options.SignIn.RequireConfirmedAccount = true if you want confirmed account email address
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+// Data Seeder for User Roles
+using (var scope = app.Services.CreateScope())
+{
+    // Initialise an instance of the roleManager
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var roles = new[] { "Admin", "Auditor", "Quality Management Team Member", "Engagement Partner", "Engagement Team Member" };
+
+    // Iterate through the roles and add them to database if they have not been created
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+            await roleManager.CreateAsync(new IdentityRole(role));
+    }
+}
+
+// Data Seeder for mapping user to roles
+using (var scope = app.Services.CreateScope())
+{
+    // Initialise an instance of the userManager
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    string email = "admin@gmail.com";
+    string password = "P@ssw0rd";
+
+    // Check if admin user has already been created
+    if (await userManager.FindByEmailAsync(email) == null)
+    {
+        // Set details of the admin user
+        var user = new IdentityUser();
+        user.UserName = email;
+        user.Email = email;
+
+        // Create a new admin account asynchronously
+        await userManager.CreateAsync(user, password);
+
+        await userManager.AddToRoleAsync(user, "Admin");
+    }
+
+}
+
+app.Run();
