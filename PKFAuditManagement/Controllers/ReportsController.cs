bb0ce9500d92c026d1feeab3cb6979336e1911e7
name54321 <2201938@sit.singaporetech.edu.sg>
2024-07-11 02:05:44 +0800

    QC7 displayed and additional columns added to GenerateReports

--- PKFAuditManagement/Controllers/ReportsController.cs
+++ PKFAuditManagement/Controllers/ReportsController.cs
@@ -20,10 +20,11 @@ namespace PKFAuditManagement.Controllers
         {
             // Step 1: Retrieve the QC6 forms from the database
             var qc6Forms = _context.QC6Forms.ToList();
-
+            var qc7Forms = _context.QC7Forms.ToList();
             // Step 2: Map the data to the ViewModel
-            var reportData = qc6Forms.Select(qc6Form => new ReportsViewModel
+            var reportDataQC6 = qc6Forms.Select(qc6Form => new ReportsViewModel
             {
+                FormType = "QC6",
                 QC6FormID = qc6Form.QC6FormID,
                 FileReference = qc6Form.FileReference,
                 ProspectiveClient = qc6Form.ProspectiveClient,
@@ -55,6 +56,43 @@ namespace PKFAuditManagement.Controllers
                 PublicInterestEntityType = qc6Form.PublicInterestEntityType,
                 // Add other fields as needed
             }).ToList();
+
+            var reportDataQC7 = qc7Forms.Select(qc7Form => new ReportsViewModel
+            {
+                FormType = "QC7",
+                QC7FormID = qc7Form.QC7FormID,
+                FileReference = qc7Form.FileReference,
+                ProspectiveClient = qc7Form.Client,
+                PeriodEnded = qc7Form.PeriodEnded,
+                EngagementType = qc7Form.EngagementType,
+                PreparedBy = qc7Form.PreparedBy,
+                PreparedByDate = qc7Form.PreparedByDate,
+                ReviewedBy = qc7Form.ReviewedBy,
+                ReviewedByDate = qc7Form.ReviewedByDate,
+                Status = qc7Form.Status,
+                RejectionReason = qc7Form.RejectionReason,
+                FormSubmissionDate = qc7Form.FormSubmissionDate,
+                PKFEntityProposingService = qc7Form.EngagementType,
+                SourceOfReferral = qc7Form.Client,
+                NatureOfServiceForEstimateFee = qc7Form.TypeOfClientActivities,
+                EstimatedFee = qc7Form.ProposedFeeCurrentYear,
+                BudgetedTimeCost = qc7Form.BudgetedTimeCost,
+                BudgetedFeeRecoveryRate = qc7Form.ProposedRecoveryRateCurrentYear,
+                OutstandingUnpaidFees = qc7Form.AnyOutstandingUnpaidAuditFees,
+                AuditFee = qc7Form.PriorYearFee,
+                GrandTotal = qc7Form.TimeCosts, // Example calculation
+                FeeConcentration = qc7Form.FeeConcentration,
+                ConflictsCheckDone = qc7Form.AnySuspiciousTransactionReportFiled,
+                TypeOfActivities = qc7Form.TypeOfClientActivities,
+                ComplexityOfEngagement = qc7Form.RiskRatingPriorYear,
+                PredecessorAuditor = qc7Form.SafeguardReviewerName,
+                ReasonsForDiscontinuance = qc7Form.SuspiciousTransactionReportFiledComment,
+                IsPublicInterestEntity = qc7Form.IsPublicInterestEntity,
+                PublicInterestEntityType = qc7Form.PublicInterestEntityType,
+                // Add other fields as needed
+            }).ToList();
+
+            var reportData = reportDataQC6.Concat(reportDataQC7).ToList();
 
             // Step 3: Pass the report data to the view
             return View("~/Views/General/Reports/GenerateReports.cshtml", reportData);
--- PKFAuditManagement/ViewModels/ReportsViewModel.cs
+++ PKFAuditManagement/ViewModels/ReportsViewModel.cs
@@ -5,11 +5,13 @@ namespace PKFAuditManagement.ViewModels
 {
     public class ReportsViewModel
     {
-        // QC6 Form properties
+        public string FormType { get; set; } // "QC6" or "QC7"
+
+        // QC6 properties
         public int QC6FormID { get; set; }
         public string FileReference { get; set; }
         public string ProspectiveClient { get; set; }
-        public DateTime? PeriodEnded { get; set; }
+        public DateTime PeriodEnded { get; set; }
         public string EngagementType { get; set; }
         public string PreparedBy { get; set; }
         public DateTime PreparedByDate { get; set; }
@@ -21,12 +23,12 @@ namespace PKFAuditManagement.ViewModels
         public string PKFEntityProposingService { get; set; }
         public string SourceOfReferral { get; set; }
         public string NatureOfServiceForEstimateFee { get; set; }
-        public decimal? EstimatedFee { get; set; }
-        public decimal? BudgetedTimeCost { get; set; }
-        public decimal? BudgetedFeeRecoveryRate { get; set; }
+        public decimal EstimatedFee { get; set; }
+        public decimal BudgetedTimeCost { get; set; }
+        public decimal BudgetedFeeRecoveryRate { get; set; }
         public bool OutstandingUnpaidFees { get; set; }
-        public decimal? AuditFee { get; set; }
-        public decimal? GrandTotal { get; set; }
+        public decimal AuditFee { get; set; }
+        public decimal GrandTotal { get; set; }
         public decimal? FeeConcentration { get; set; }
         public bool ConflictsCheckDone { get; set; }
         public string TypeOfActivities { get; set; }
@@ -36,6 +38,34 @@ namespace PKFAuditManagement.ViewModels
         public bool IsPublicInterestEntity { get; set; }
         public string PublicInterestEntityType { get; set; }
 
+        // QC7 properties
+        public int QC7FormID { get; set; }
+        public string Client { get; set; }
+        public DateTime PeriodEndedQC7 { get; set; }
+        public string EngagementTypeQC7 { get; set; }
+        public string PreparedByQC7 { get; set; }
+        public DateTime PreparedByDateQC7 { get; set; }
+        public string ReviewedByQC7 { get; set; }
+        public DateTime ReviewedByDateQC7 { get; set; }
+        public string StatusQC7 { get; set; }
+        public string RejectionReasonQC7 { get; set; }
+        public DateTime FormSubmissionDateQC7 { get; set; }
+        public decimal PriorYearFee { get; set; }
+        public decimal TimeCosts { get; set; }
+        public decimal PriorYearRecoveryRate { get; set; }
+        public bool AnyOutstandingUnpaidAuditFees { get; set; }
+        public string TypeOfClientActivities { get; set; }
+        public string RiskRatingPriorYear { get; set; }
+        public bool AnySuspiciousTransactionReportFiled { get; set; }
+        public string SuspiciousTransactionReportFiledComment { get; set; }
+        public string SafeguardReviewerName { get; set; }
+        public decimal ProposedFeeCurrentYear { get; set; }
+        public decimal BudgetedTimeCostQC7 { get; set; }
+        public decimal ProposedRecoveryRateCurrentYear { get; set; }
+        public bool IsPublicInterestEntityQC7 { get; set; }
+        public string PublicInterestEntityTypeQC7 { get; set; }
+        public bool IsSubForm2NotApplicable { get; set; }
+        public bool IsSubForm3NotApplicable { get; set; }
         // Add other properties if needed
     }
 }
