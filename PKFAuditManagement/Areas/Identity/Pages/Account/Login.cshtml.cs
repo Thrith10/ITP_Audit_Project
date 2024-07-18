07c267036e6a12a7f48bf24e91e3c935c43fa682
russelpwq <russelpoon25@gmail.com>
2024-07-18 12:58:55 +0800

    Bug fix

--- PKFAuditManagement/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PKFAuditManagement/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -68,7 +68,6 @@ namespace PKFAuditManagement.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [EmailAddress]
             public string Email { get; set; }
 
             /// <summary>
