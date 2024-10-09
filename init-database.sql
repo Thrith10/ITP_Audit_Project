76a627c2a80309ece62092ba1c958b304a17f158
russelpwq <2201319@sit.singaporetech.edu.sg>
2024-10-09 20:25:40 +0800

    Update sql script

--- init-database.sql
+++ init-database.sql
@@ -1,5 +1,5 @@
 -- Create database if not exists
-CREATE DATABASE IF NOT EXISTS `pkf_audit_database`;
+CREATE DATABASE IF NOT EXISTS `new_database`;
 
 -- Use the database
-USE `pkf_audit_database`;
+USE `new_database`;
